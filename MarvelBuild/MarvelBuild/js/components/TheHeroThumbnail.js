71a87e1858bdd031c19265e2182385587a7848e5
Your Name <you@example.com>
2022-11-01 12:06:26 -0400

    setup

---
+++ MarvelBuild/MarvelBuild/js/components/TheHeroThumbnail.js
@@ -0,0 +1,22 @@
+export default {
+    name: 'TheHeroThumb',
+
+    props: {
+        hero: Object
+    },
+
+    template:
+    `
+    <li>
+        <!--<img class="sprite" src="" alt="hero image">-->
+        <div class="sprite"></div>
+        <div class="red-bumper"></div>
+        <h5 class="">name</h5>
+    </li>
+    `
+
+
+
+}
+
+
--- MarvelBuild/MarvelBuild/js/main.js
+++ MarvelBuild/MarvelBuild/js/main.js
@@ -4,7 +4,11 @@
 
     createApp({
         created() {
-            
+           // fetch calls always go here -->retrieve any data you need
+           fetch('./data.json')//go and get the remote data
+             .then(res => res.json())//convert the json object to plain JS object
+             .then(data => this.heroData = data)//store the data un the vue instance
+            .catch(error => console.error(error)); //report any errors
         },
 
         data() {
